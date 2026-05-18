72e9744e18f08fa6dadb206c4fa1f1b4e789145c
Wei Zhang <vivian99zh@googlemail.com>
2026-05-18 10:09:24 +0200

    Use map to create winRules array

--- rockPaperScissors.js
+++ rockPaperScissors.js
@@ -1,21 +1,13 @@
 const choices = ["rock", "paper", "scissors"];
 const userChoice = process.argv[2]?.toLowerCase();
-const computerChoice = choices[Math.floor(Math.random() * choices.length)];
-
-const getWinRules = (choices) => {
-  const rules = [];
-  for (let i = 0; i < choices.length; i++) {
-    rules.push(`${choices[i]}-${choices[i - 1 < 0 ? choices.length - 1 : i - 1]}`);
-  }
-  return rules;
-};
-
-const winRules = getWinRules(choices);
 
 if (!userChoice || !choices.includes(userChoice) || process.argv.length > 3) {
-  console.log(`Please only choose: ${choices.join(", ")}`);
+  console.log(`Please only choose: ${choices.join(", ")}!`);
   process.exit(1);
 }
+
+const computerChoice = choices[Math.floor(Math.random() * choices.length)];
+const winRules = choices.map((_, i, arr) => `${arr[i]}-${arr[i - 1 < 0 ? arr.length - 1 : i - 1]}`);
 
 let resultMsg = "You lose!";
 if (userChoice === computerChoice) {
